f1d11aef618a9c682955242ee977da656efc1d33
Roel van Lanen <roelvanlanen@gmail.com>
2018-11-09 17:26:30 +0100

    Dynamic import example

--- src/features/parent.js
+++ src/features/parent.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { Child } from '../elements/child';
 import styles from './parent.module.scss';
+import Loadable from 'react-loadable';
+
+const Child = Loadable({
+    loader: () => import('../elements/child'),
+    render(loaded, props) {
+        let Component = loaded.Child;
+        return <Component {...props} />;
+    },
+    loading() {
+        return <div>Loading...</div>;
+    },
+});
 
 export const Parent = () => (
     <div className={styles.base}>
